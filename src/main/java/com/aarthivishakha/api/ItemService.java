34724e1ce5c2a408954332bdcdd4cc46f9ca4aa7
Aarthivishakha <aarthivishakha@gmail.com>
2026-08-13 10:18:23 +0530

    Modernize Quarkus REST API for Java 11

--- src/main/java/com/aarthivishakha/api/ItemService.java
+++ src/main/java/com/aarthivishakha/api/ItemService.java
@@ -5,7 +5,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
-import javax.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class ItemService {
