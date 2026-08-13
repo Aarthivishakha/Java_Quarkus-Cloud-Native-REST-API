5895e4681b9ce8b34e382e7aaa6f68eb4cdf8647
Aarthivishakha <aarthivishakha@gmail.com>
2026-08-13 10:18:23 +0530

    Modernize Quarkus REST API for Java 11

--- src/main/java/com/aarthivishakha/api/HealthResource.java
+++ src/main/java/com/aarthivishakha/api/HealthResource.java
@@ -2,10 +2,10 @@ package com.aarthivishakha.api;
 
 import java.util.Collections;
 import java.util.Map;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/health") @Produces(MediaType.APPLICATION_JSON)
 public class HealthResource {
